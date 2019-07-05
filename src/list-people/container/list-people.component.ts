8455e48b052060b78e843551cd826532654b6190
Adrian Ferreres <adrian.ferreres@sngular.com>
2019-07-05 14:47:39 +0200

    Add home with table

---
+++ src/list-people/container/list-people.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ardi-list-people',
+  templateUrl: './list-people.component.html',
+  styleUrls: ['./list-people.component.css']
+})
+export class ListPeopleComponent {}
