0f8cda1bb7cb272aefe8dab100b639e966d9bf79
Adrian Ferreres <adrian.ferreres@sngular.com>
2019-07-11 14:15:52 +0200

    Add Selectors

---
+++ src/list-people/selectors/list-people.selector.ts
@@ -0,0 +1,23 @@
+import { createSelector } from '@ngrx/store';
+import { PeopleState } from '../reducer';
+
+const configFeature = (state: PeopleState) => state.config;
+const dataFeature = (state: PeopleState) => state.data;
+const pageLimitFeature = (state: PeopleState) => ({
+  page: state.page,
+  limit: state.limit
+});
+
+
+export const configSelector = createSelector(
+  configFeature,
+  (state: {[key: string]: string}[]) => state
+);
+export const dataSelector = createSelector(
+  dataFeature,
+  (state: {[key: string]: any}[]) => state
+);
+export const pageLimitSelector = createSelector(
+  pageLimitFeature,
+  (state:{page: number, limit: number}) => state
+);
