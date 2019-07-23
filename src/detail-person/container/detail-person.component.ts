ed3fcd8a1aa4b2da972fd01dc96fba5fa4f46a02
Adrian Ferreres <adrian.ferreres@sngular.com>
2019-07-23 18:45:44 +0200

    Add frame component
    - Create frame component
    - Create featuer detail-person
    - Create containter for detail-person
    - Add the frame in the container
    - Import the container in the details web page

---
+++ src/detail-person/container/detail-person.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ardi-detail-person',
+  templateUrl: './detail-person.component.html',
+  styleUrls: ['./detail-person.component.css']
+})
+export class DetailPersonComponent {}
