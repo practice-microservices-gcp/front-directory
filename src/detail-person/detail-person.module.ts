3813e146e9865b5d8b090b09c07204672ebe9088
Adrian Ferreres <adrian.ferreres@sngular.com>
2019-08-02 22:09:03 +0200

    Add module and integration with the container

--- src/detail-person/detail-person.module.ts
+++ src/detail-person/detail-person.module.ts
@@ -3,10 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { FrameComponent, ListPropertiesComponent } from '../components';
 import { DetailPersonComponent } from './container';
+import { StoreModule } from '@ngrx/store';
+import { FEATURE_DETAIL_PEOPLE } from './selectors';
+import { reducer } from './reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { DetailsPeopleEffects } from './effects';
 
 @NgModule({
   declarations: [ FrameComponent, ListPropertiesComponent, DetailPersonComponent ],
-  imports: [ CommonModule ],
+  imports: [
+    CommonModule,
+    StoreModule.forFeature(FEATURE_DETAIL_PEOPLE, reducer),
+    EffectsModule.forFeature([DetailsPeopleEffects])
+  ],
   exports: [ DetailPersonComponent ]
 })
 export class DetailPersonModule {}
